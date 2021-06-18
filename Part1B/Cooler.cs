a0bdfa0147e4fe6a2dbcfffeacee0ac77b05fc48
Raw <tranduc@gmail.com>
2021-06-18 11:48:02 +0700

    raw

---
+++ Part1B/Cooler.cs
@@ -0,0 +1,27 @@
+﻿namespace Part1B
+{
+    public class Cooler
+    {
+        public Cooler(float temperature)
+        {
+            Temperature = temperature;
+        }
+        public float Temperature
+        {
+            get { return _Temperature; }
+            set { _Temperature = value; }
+        }
+        private float _Temperature;
+        public void OnTemperatureChanged(float newTemperature)
+        {
+            if (newTemperature > Temperature)
+            {
+                System.Console.WriteLine("Cooler: On");
+            }
+            else
+            {
+                System.Console.WriteLine("Cooler: Off");
+            }
+        }
+    }
+}
